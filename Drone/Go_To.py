9f93249f45dce45fc6e0008ec9b661e0f70bbda6
nahom2225 <112717460+nahom2225@users.noreply.github.com>
2022-11-11 12:53:36 -0600

    Add files via upload
    Function with allows for a sequence of states to go to with mapping

---
+++ Drone/Go_To.py
@@ -0,0 +1,84 @@
+from djitellopy import tello
+
+import KeyPressModule as kp
+
+import numpy as np
+
+from time import sleep
+
+import cv2
+
+import math
+
+import sys
+
+me = tello.Tello()
+me.connect()
+print(me.get_battery())
+
+points = [(0, 0), (0, 0)]
+
+
+# Function to take coordinate input
+def goto(goal_x, goal_y, take_off):
+    # Starting position is 0,0
+    # yaw = -65 is pointing north
+    to_rotate = -65 - (me.get_yaw())
+
+    # Calculate angle and distance
+    angle = int(math.degrees(math.atan(goal_y / goal_x)))
+    dist = int(math.sqrt((goal_x ** 2) + (goal_y ** 2)))
+    # print(angle, dist)
+
+    # 1. Rotate drone to desired coordinate
+    # 2. Fly in straight line (forward)
+    if take_off == 1:
+        me.takeoff()
+        me.rotate_counter_clockwise(to_rotate)
+        me.rotate_counter_clockwise(angle)
+        me.move_forward(dist)
+    elif take_off == 0:
+        me.rotate_counter_clockwise(angle)
+        me.move_forward(dist)
+    elif take_off == -1:
+        me.land()
+
+
+def drawPoints(img, points):
+    for point in points:
+        cv2.circle(img, point, 5, (0, 0, 255), cv2.FILLED)
+
+    cv2.circle(img, points[-1], 8, (0, 255, 0), cv2.FILLED)
+
+    cv2.putText(img, f'({(points[-1][0] - 500) / 100},{(points[-1][1] - 500) / 100})m',
+
+                (points[-1][0] + 10, points[-1][1] + 30), cv2.FONT_HERSHEY_PLAIN, 1,
+
+                (255, 0, 255), 1)
+
+
+if __name__ == '__main__':
+    # Extracting user input for coords
+    while True:
+        # Setting up mapping image
+        img = np.zeros((1000, 1000, 3), np.uint8)
+        cur_angle = me.get_yaw()
+        cur_dist = me.get_distance_tof()
+        cur_x = cur_angle * math.cos(cur_dist)
+        cur_y = cur_angle * math.sin(cur_dist)
+        points.append((cur_x, cur_y))
+        print(cur_angle, cur_dist, points)
+        drawPoints(img, points)
+        cv2.imshow("Output", img)
+        cv2.waitKey(0)
+        # User input for coordinates
+        coordinates = str(input())
+        coords = coordinates.split(',')
+        x = int(coords[0].strip())
+        y = int(coords[1].strip())
+        take_off = int(coords[2].strip())
+        # Go to state if exists
+        if x and y and (1 >= take_off >= -1):
+            goto(x, y, take_off)
+        # Getting current location to add point to image
+
